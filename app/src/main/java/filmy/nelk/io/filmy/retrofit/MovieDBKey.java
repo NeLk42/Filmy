3089deeb47e20ded7b4ea3c4b5b44a48a2b9aaf5
NeLk42 <nelk42@gmail.com>
2017-04-16 19:51:57 +0100

    :lock: Adding fake API Key, adding it to gitignore so that it doesn’t update the document with the real key.

---
+++ app/src/main/java/filmy/nelk/io/filmy/retrofit/MovieDBKey.java
@@ -0,0 +1,8 @@
+package filmy.nelk.io.filmy.retrofit;
+
+public class MovieDBKey {
+
+    // The following key is fake, a random string which resembles the actual key.
+    public static final String apiKey = "0e461f41c9e71674ba700bce5214af7c";
+
+}
