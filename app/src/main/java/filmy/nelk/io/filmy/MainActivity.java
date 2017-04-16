51aa2aa3bdeabf02323f2323f26d7d8c3337b398
NeLk42 <nelk42@gmail.com>
2017-04-16 19:26:33 +0100

    :tada: Basic Android project

---
+++ app/src/main/java/filmy/nelk/io/filmy/MainActivity.java
@@ -0,0 +1,13 @@
+package filmy.nelk.io.filmy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
