8701d2596d5d0e3a4798deb031c05dc77e86cdcf
NeLk42 <nelk42@gmail.com>
2017-04-16 19:55:46 +0100

    :pencil2: Constant

--- app/src/main/java/filmy/nelk/io/filmy/retrofit/MovieDBKey.java
+++ app/src/main/java/filmy/nelk/io/filmy/retrofit/MovieDBKey.java
@@ -3,6 +3,6 @@ package filmy.nelk.io.filmy.retrofit;
 public class MovieDBKey {
 
     // The following key is fake, a random string which resembles the actual key.
-    public static final String apiKey = "0e461f41c9e71674ba700bce5214af7c";
+    public static final String MOVIEDB_APIKEY = "0e461f41c9e71674ba700bce5214af7c";
 
 }
