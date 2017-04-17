c6479615a9203d134572c2f441b4f14d15d6e91d
NeLk42 <nelk42@gmail.com>
2017-04-17 09:52:48 +0100

    :wrench: Adding config constants for mdb

---
+++ app/src/main/java/filmy/nelk/io/filmy/retrofit/APIConfig.java
@@ -0,0 +1,13 @@
+package filmy.nelk.io.filmy.retrofit;
+
+public class APIConfig {
+
+    public static final String MDB_API_KEY          = MovieDBKey.MOVIEDB_APIKEY;
+    public static final String MDB_API_KEY_PARAM    = "api_key";
+
+    public static final String MDB_BASE_URL         = "http://api.themoviedb.org/3/";
+    public static final String MDB_TOP_RATED        = "movie/top_rated";
+    public static final String MDB_POPULAR          = "movie/popular";
+    public static final String MDB_SIZE             = "w185";
+
+}
