f460a5b5cca0f255ccae23df1bab1c97a22c22d0
NeLk42 <nelk42@gmail.com>
2017-04-17 14:48:43 +0100

    :art: Moving MovieViewHolder inside MovieAdapter

--- app/src/main/java/filmy/nelk/io/filmy/recyclerview/MovieAdapter.java
+++ app/src/main/java/filmy/nelk/io/filmy/recyclerview/MovieAdapter.java
@@ -1,9 +1,15 @@
 package filmy.nelk.io.filmy.recyclerview;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder>{
+import filmy.nelk.io.filmy.R;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
     public static final String TAG = MovieAdapter.class.getSimpleName();
 
     public MovieAdapter() {
@@ -25,4 +31,17 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder>{
         return 0;
     }
 
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title;
+        public TextView rating;
+        public ImageView poster;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            this.title = (TextView) itemView.findViewById(R.id.tv_title);
+            this.rating = (TextView) itemView.findViewById(R.id.tv_rating);
+            this.poster = (ImageView) itemView.findViewById(R.id.iv_poster);
+        }
+    }
 }
