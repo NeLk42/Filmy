e8fcc2416211953779ac2a95806521d553e8206e
NeLk42 <nelk42@gmail.com>
2017-04-17 14:24:44 +0100

    :construction: Adding MovieAdapter, it’ll handle information coming from retrofit/moviedb API and bind it with the recyclerview.

---
+++ app/src/main/java/filmy/nelk/io/filmy/recyclerview/MovieAdapter.java
@@ -0,0 +1,28 @@
+package filmy.nelk.io.filmy.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder>{
+    public static final String TAG = MovieAdapter.class.getSimpleName();
+
+    public MovieAdapter() {
+
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
