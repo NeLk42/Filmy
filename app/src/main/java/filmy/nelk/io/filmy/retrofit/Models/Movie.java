7f98242dc83e2cc0ac22b1843ca4c5590edc3481
NeLk42 <nelk42@gmail.com>
2017-04-17 09:54:04 +0100

    :construction: Adding Models for Movie and MovieList JSONs

---
+++ app/src/main/java/filmy/nelk/io/filmy/retrofit/Models/Movie.java
@@ -0,0 +1,32 @@
+package filmy.nelk.io.filmy.retrofit.Models;
+
+public class Movie {
+
+    String title;
+    String overview;
+    String popularity;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+}
---
+++ app/src/main/java/filmy/nelk/io/filmy/retrofit/Models/MoviesList.java
@@ -0,0 +1,50 @@
+package filmy.nelk.io.filmy.retrofit.Models;
+
+import java.util.List;
+
+/**
+ * Created by NeLk on 16/04/2017.
+ */
+
+public class MoviesList {
+
+    public Integer page;
+
+    public List<Movie> results;
+
+    public Integer total_results;
+
+    public Integer total_pages;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public Integer getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(Integer total_results) {
+        this.total_results = total_results;
+    }
+
+    public Integer getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(Integer total_pages) {
+        this.total_pages = total_pages;
+    }
+}
